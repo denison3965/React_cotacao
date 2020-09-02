fe570d52e73a959a8e0f34acc4e69c917856f445
Denison <denisondealmeidaprtela@hotmail.com>
2020-09-02 02:11:14 -0300

    terminando o componente gáfico

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
+import Grafico from './components/grafico'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Grafico></Grafico>
   </React.StrictMode>,
   document.getElementById('root')
 );
