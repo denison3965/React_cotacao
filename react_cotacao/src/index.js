ea875fa37487211011f4f2ba2f5adcfbc9798933
Ana204 <60896884+Ana204@users.noreply.github.com>
2020-09-03 04:27:55 -0300

    subindo o header e o footer

--- react_cotacao/src/index.js
+++ react_cotacao/src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer from './components2/footer';
+import Header from './components2/header';
+import './components2/footer.css';
+import './components2/header.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Header />
+    <Footer />
   </React.StrictMode>,
   document.getElementById('root')
 );
