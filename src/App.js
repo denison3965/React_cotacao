e49bfd369e45340697854bf2059012354df64603
Denison <denisondealmeidaprtela@hotmail.com>
2020-09-03 04:20:20 -0300

    Fazendo pagina de dev e pagina de sobre o projeto

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Grafico from './components/grafico';
 import Cotacao from './componente3/cotacao';
 import SobreNos from './components/SobreNos';
+import Desenvolvedores from './components/Desenvolvedores'
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 
@@ -14,7 +15,7 @@ export default () =>{
 
       <BrowserRouter> 
       <div className="container" style={{padding: "30px 30px"}}>
-      <Link style={{color: "#40E0D0"}} to="/components/grafico.js">IBOVESPA</Link> | <Link style={{color: "#40E0D0"}} to="/components3/cotacao.js">Cotacão de Moedas</Link> | <Link style={{color: "#40E0D0"}} to="/components/SobreNos.js">Sobre o projeto</Link>
+      <Link style={{color: "#40E0D0"}} to="/components/grafico.js">IBOVESPA</Link> | <Link style={{color: "#40E0D0"}} to="/components3/cotacao.js">Cotacão de Moedas</Link> | <Link style={{color: "#40E0D0"}} to="/components/SobreNos.js">Sobre o projeto</Link> | <Link style={{color: "#40E0D0"}} to="/components/Desenvolvedores.js">Desenvolvedores</Link>
       </div>
 
        
@@ -23,6 +24,7 @@ export default () =>{
         <Route path="/components/grafico.js" exact component={Grafico}></Route>
         <Route path="/components3/cotacao.js" component={Cotacao}></Route>
         <Route path="/components/SobreNos.js" component={SobreNos}></Route>
+        <Route path="/components/Desenvolvedores.js" component={Desenvolvedores}></Route>
       </BrowserRouter>
     </div>
   );
